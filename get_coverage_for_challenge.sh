ff5eef7692e980b184c2e818dadac2c9089da41a
mani <sadhak001@gmail.com>
2018-05-09 16:15:57 +0100

    Applying code review comments: outputs redirected to stderr instead stdout, using xmllint instead of cat, awk, grep

--- get_coverage_for_challenge.sh
+++ get_coverage_for_challenge.sh
@@ -6,15 +6,9 @@ set -o pipefail
 
 SCRIPT_CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-challengeId=$1
-JACOCO_TEST_REPORT_CSV_FILE="${SCRIPT_CURRENT_DIR}/build/reports/jacoco/test/jacocoTestReport.csv"
+CHALLENGE_ID=$1
+JACOCO_TEST_REPORT_CSV_FILE="${SCRIPT_CURRENT_DIR}/build/reports/jacoco/test/jacocoTestReport.xml"
 
-./gradlew clean test jacocoTestReport 1>&2 || true
+./gradlew -q clean test jacocoTestReport || true 1>&2
 
-# $2 - position where the package name containing the challenge id is located in the CSV file
-# $9 - position where the line covered information for the challenge id is located in the CSV file
-# Note: these two positions could change if a newer update of Jacoco changes the format of the CSV file
-cat "${JACOCO_TEST_REPORT_CSV_FILE}" \
-	              | awk -F, 'NR > 1 { print $2 "," $9 }' \
-                  | grep "${challengeId}" \
-                  | awk '{split($0, a, ","); print a[2]}'
+xmllint --xpath '//package[@name="befaster/solutions/'${CHALLENGE_ID}'"]/counter[@type="INSTRUCTION"]' ${JACOCO_TEST_REPORT_CSV_FILE} 1>&2
