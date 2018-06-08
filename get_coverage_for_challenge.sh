8d484df86115ddf926d0b6d3255fab3c29689115
neomatrix369 <sadhak001@gmail.com>
2018-06-08 22:34:38 +0100

    Fixed an edge-case to return 0 when the challenge_id passed in has no tests at all or the challenge_id is incorrect/does not exists

--- get_coverage_for_challenge.sh
+++ get_coverage_for_challenge.sh
@@ -20,11 +20,15 @@ export GRADLE_OPTS=${GRADLE_OPTS:-""}
 [ -e ${JAVA_CODE_COVERAGE_INFO} ] && rm ${JAVA_CODE_COVERAGE_INFO}
 
 if [ -f "${JACOCO_TEST_REPORT_XML_FILE}" ]; then
-    COVERAGE_OUTPUT=$(xmllint --xpath '//package[@name="befaster/solutions/'${CHALLENGE_ID}'"]/counter[@type="INSTRUCTION"]' ${JACOCO_TEST_REPORT_XML_FILE})
-    MISSED=$(echo $COVERAGE_OUTPUT | awk '{print missed, $3}' | tr '="' ' ' | awk '{print $2}')
-    COVERED=$(echo $COVERAGE_OUTPUT | awk '{print missed, $4}' | tr '="' ' '| awk '{print $2}')
-    TOTAL_LINES=$((MISSED + $COVERED))
-    echo $(($COVERED * 100 / $TOTAL_LINES)) > ${JAVA_CODE_COVERAGE_INFO}
+    COVERAGE_OUTPUT=$(xmllint --xpath '//package[@name="befaster/solutions/'${CHALLENGE_ID}'"]/counter[@type="INSTRUCTION"]' ${JACOCO_TEST_REPORT_XML_FILE} || true)
+    PERCENTAGE=$(( 0 ))
+    if [[ ! -z "${COVERAGE_OUTPUT}" ]]; then
+        MISSED=$(echo $COVERAGE_OUTPUT | awk '{print missed, $3}' | tr '="' ' ' | awk '{print $2}')
+        COVERED=$(echo $COVERAGE_OUTPUT | awk '{print missed, $4}' | tr '="' ' '| awk '{print $2}')
+        TOTAL_LINES=$((MISSED + $COVERED))
+        PERCENTAGE=$(($COVERED * 100 / $TOTAL_LINES))
+    fi
+    echo ${PERCENTAGE} > ${JAVA_CODE_COVERAGE_INFO}
     cat ${JAVA_CODE_COVERAGE_INFO}
     exit 0
 else
